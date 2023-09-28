19a3235e92502837186c2edad60197cd15660b8f
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-09-28 22:53:14 +0530

    replaced quotes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,11 @@ interface Props {
     query: string;
     category: string;
 }
-const Header = ({  query, category }: Props) => {
+const Header = ({ query, category }: Props) => {
     if (query && category) {
         return (
             <h1 className="heading3 self-start text-white-800">
-                Search Results for "{query}" in{" "}
+                Search Results for &quot;{query}&quot; in
                 <span className="capitalize">{category}</span>
             </h1>
         );
@@ -16,14 +16,14 @@ const Header = ({  query, category }: Props) => {
     if (query) {
         return (
             <h1 className="heading3 self-start text-white-800">
-                Search Results for "{query}"
+                Search Results for &quot;{query}&quot;
             </h1>
         );
     }
     if (category) {
         return (
             <h1 className="heading3 self-start text-white-800">
-                Search Results for "{category}"
+                Search Results for &quot;{category}&quot;
             </h1>
         );
     }
